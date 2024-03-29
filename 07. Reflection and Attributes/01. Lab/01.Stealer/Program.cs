53b316d4bbe677e67db30f77bfe768ece1f3d88f
Iliyana Dzhabirova <il.dzhabirova@abv.bg>
2024-03-29 22:14:55 +0200

    Upload Reflection and Attributes

---
+++ 07. Reflection and Attributes/01. Lab/01.Stealer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Stealer;
+public class Program
+{
+    public static void Main()
+    {
+        Spy spy= new Spy();
+        string result=spy.StealFieldInfo("Stealer.Hacker", "username",  "password");
+        Console.WriteLine(result);
+    }
+}
---
+++ 07. Reflection and Attributes/02. Exercise/ValidationAttributes/Model/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Model
+{
+    public class Person
+    {
+        private const int MinAge = 12;
+        private const int MaxAge = 90;
+        [MyRequired]
+        public string FullName {  get; set; }
+        [MyRange(MinAge, MaxAge)]
+        public int Age { get; set; }
+        public Person(string fullName, int age)
+        {
+            FullName = fullName;
+            Age = age;
+        }
+    }
+}
